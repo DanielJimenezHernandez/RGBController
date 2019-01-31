8939093ca3453a1bd3f5e5e8eb1068594ac2d5f2
Daniel Jimenez <danieljimher@hotmail.es>
2019-01-31 02:10:52 -0600

    Added gamma correction
    Used a generic Gamma lookup table to add some color correction for human eye perception

--- components/led_control/led_control.c
+++ components/led_control/led_control.c
@@ -37,6 +37,25 @@ d19 = B
  *    GPIO4/5 are from low speed channel group.
  *
  */
+
+const uint8_t gamma8[] = {
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+    1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+    2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+    5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+   10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+   17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+   25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+   37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+   51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+   69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+   90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+  115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+  144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+  177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+  215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
+
 #define LEDC_HS_TIMER          LEDC_TIMER_0
 #define LEDC_HS_MODE           LEDC_HIGH_SPEED_MODE
 #define LEDC_HS_R_GPIO         (5)
@@ -129,10 +148,11 @@ void set_color(uint8_t r,uint8_t g,uint8_t b){
     /* color sanity check*/
     ESP_LOGI(TAG, "Setting - r[%d] g[%d] b[%d]",r,g,b);
 
+
     /* convert color to duty*/
-    uint32_t r_duty = r * brightness_limit / 255;
-    uint32_t g_duty = g * brightness_limit / 255;
-    uint32_t b_duty = b * brightness_limit / 255;
+    uint32_t r_duty = gamma8[r] * brightness_limit / 255;
+    uint32_t g_duty = gamma8[g] * brightness_limit / 255;
+    uint32_t b_duty = gamma8[b] * brightness_limit / 255;
         
     if(ledc_set_duty(ledc_channel[LEDC_R].speed_mode, ledc_channel[LEDC_R].channel, r_duty) == ESP_OK){
             if (ledc_update_duty(ledc_channel[LEDC_R].speed_mode, ledc_channel[LEDC_R].channel) == ESP_OK){
@@ -176,9 +196,9 @@ void fade_to_color(uint8_t r,uint8_t g,uint8_t b){
 
 
     /* convert color to duty*/
-    uint32_t r_duty = r * brightness_limit / 255;
-    uint32_t g_duty = g * brightness_limit / 255;
-    uint32_t b_duty = b * brightness_limit / 255;
+    uint32_t r_duty = gamma8[r] * brightness_limit / 255;
+    uint32_t g_duty = gamma8[g] * brightness_limit / 255;
+    uint32_t b_duty = gamma8[b] * brightness_limit / 255;
 
     ledc_set_fade_with_time(ledc_channel[LEDC_R].speed_mode, ledc_channel[LEDC_R].channel,r_duty,500);
     ledc_set_fade_with_time(ledc_channel[LEDC_G].speed_mode, ledc_channel[LEDC_G].channel,g_duty,500);
