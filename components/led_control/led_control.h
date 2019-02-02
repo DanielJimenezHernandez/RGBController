d1e91c30f5cfe3d63a0d2b6d6d0a06df546b0ce4
Daniel Jimenez <danieljimher@hotmail.es>
2019-02-02 02:29:58 -0600

    Major changes in led component some refactoring on mqtt
    *Added 13bit gamma lookup table
    *Refactored some code
    *modified mqtt component to pass the complete event

--- components/led_control/led_control.h
+++ components/led_control/led_control.h
@@ -1,6 +1,11 @@
 #ifndef LED_CONTROL_H
 #define LED_CONTROL_H
 
+#define LEDC_CH_NUM       (3)
+#define LEDC_R             0
+#define LEDC_G             1
+#define LEDC_B             2
+
 typedef enum led_mode_ {
     LED_MODE_CONNECTING_TO_AP,
     LED_MODE_READY_FOR_CONFIG,
@@ -17,19 +22,17 @@ typedef enum led_mode_ {
 typedef struct {
     uint32_t duty;
     uint8_t hex_val;
-}led_info;
+}led_channel_config_t;
 
-typedef struct {
+typedef struct{
     eLed_mode mode;
-    uint32_t bightness;      /*configured brignthness*/
-    uint32_t fadetime;
-    led_info r;
-    led_info g;
-    led_info b;
-} sLed_state;
+    uint32_t bightness;
+    uint16_t fadetime_s;
+    led_channel_config_t channel[LEDC_CH_NUM];
+}led_strip_config_t;
 
 
 void led_control_init();
-void change_mode(sLed_state *rgb_config);
+void change_mode(led_strip_config_t *rgb_config);
 
 #endif /* LED_CONTROL_H */
